8299f9d74e88577bf01f0ab02f96b267f3ceeff9
dustin <dustin.cook@mines.sdsmt.edu>
2019-04-28 16:07:23 -0600

    train.py trains the model, main.py loads the model and runs some test images on it

---
+++ Project/main.py
@@ -0,0 +1,42 @@
+from keras.layers import Input, Dense
+from keras.models import Model
+from keras.models import load_model
+from keras.layers import Input, Dense, Conv2D, MaxPooling2D, UpSampling2D
+from keras.models import Model
+from keras import backend as K
+from keras.callbacks import TensorBoard
+
+import matplotlib.pyplot as plt
+
+from keras import regularizers
+from keras.datasets import mnist
+import numpy as np
+
+(x_train, _), (x_test, _) = mnist.load_data()
+
+x_train = x_train.astype('float32') / 255.
+x_test = x_test.astype('float32') / 255.
+x_train = np.reshape(x_train, (len(x_train), 28, 28, 1))  # adapt this if using `channels_first` image data format
+x_test = np.reshape(x_test, (len(x_test), 28, 28, 1))  # adapt this if using `channels_first` image data format
+
+noise_factor = 0.5
+x_train_noisy = x_train + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_train.shape) 
+x_test_noisy = x_test + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_test.shape) 
+
+
+
+
+autoencoder = load_model('sda.h5')
+
+# Test and display 
+decoded_imgs = autoencoder.predict(x_test_noisy)
+
+n = 10
+plt.figure(figsize=(20, 2))
+for i in range(n):
+    ax = plt.subplot(1, n, i + 1)
+    plt.imshow(decoded_imgs[i].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+plt.show()
